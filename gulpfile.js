508e4b615cf4eff2c76505b950e02b559452cb36
wrgraff <wrgraff@gmail.com>
2019-10-16 16:16:30 +0300

    added browserSync instead livereload

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
     clone = require('gulp-clone'),
     concat = require('gulp-concat'),
     include = require("gulp-include"),
-    livereload = require('gulp-livereload');
+    browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/style.scss')
@@ -21,10 +21,7 @@ gulp.task('sass', function () {
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/static/css'))
-        .pipe(livereload());
-});
-gulp.task('html', function () {
-    return gulp.src('src/**/*.html').pipe(livereload());
+        .pipe(browserSync.stream());
 });
 gulp.task('js', function() {
     return gulp.src('src/js/scripts.js')
@@ -34,12 +31,15 @@ gulp.task('js', function() {
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/static/js'))
-        .pipe(livereload());
+        .pipe(browserSync.stream());
 });
 
+
 gulp.task('default', function () {
-    livereload.listen();
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('src/**/*.html', gulp.series('html'));
-    gulp.watch('src/js/**/*.js', gulp.series('js'));
+	browserSync.init({
+        server: "./dist"
+    });
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass')).on('change', browserSync.reload);
+    gulp.watch('dist/**/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', gulp.series('js')).on('change', browserSync.reload);
 });
