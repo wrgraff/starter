a5ba5855168b7550b8d183812c3f5c4475daed02
wrgraff <wrgraff@gmail.com>
2020-06-13 02:04:39 +0300

    fixed script

--- src/js/_utils.js
+++ src/js/_utils.js
@@ -1,13 +1,9 @@
 (function() {
 	window.utils = {
 		addElement: function(elTag, classes) {
-		    let element = document.createElement(elTag);
-			let classesArray = classes.split(' ');
-
-			for (elClass in classesArray) {
-				element.classList.add(elClass);
-			};
-
+			let element = document.createElement(elTag);
+			if (classes) element.className = classes;
+			
 		    return element;
 		}
 	};
