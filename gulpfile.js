2392e2e917c540ba3f18ce97ba2c0f2c3458a8db
wrgraff <wrgraff@gmail.com>
2020-06-13 02:17:33 +0300

    updates for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-	plumber = require('gulp-plumber'),
-	csso = require('gulp-csso'),
-	postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    uglify = require('gulp-uglify-es').default,
-    rename = require('gulp-rename'),
-	nunjucks = require('gulp-nunjucks-render'),
-    clone = require('gulp-clone'),
-	prettier = require('gulp-prettier'),
-	typograf = require('gulp-typograf'),
-    concat = require('gulp-concat'),
-	imagemin = require('gulp-imagemin'),
-	webp = require('gulp-webp'),
-	del = require('del'),
-	imageResize = require('gulp-image-resize'),
-	babel = require('gulp-babel'),
-    browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  plumber = require('gulp-plumber'),
+	  csso = require('gulp-csso'),
+	  postcss = require('gulp-postcss'),
+	  autoprefixer = require('autoprefixer'),
+	  uglify = require('gulp-uglify-es').default,
+	  rename = require('gulp-rename'),
+	  nunjucks = require('gulp-nunjucks-render'),
+	  clone = require('gulp-clone'),
+	  prettier = require('gulp-prettier'),
+	  typograf = require('gulp-typograf'),
+	  concat = require('gulp-concat'),
+	  imagemin = require('gulp-imagemin'),
+	  webp = require('gulp-webp'),
+	  del = require('del'),
+	  imageResize = require('gulp-image-resize'),
+	  babel = require('gulp-babel'),
+	  browserSync = require('browser-sync').create();
 
-// gulp.task('cut-img', () => {
-// 	return gulp.src('src/img/name/*.jpg')
-// 		.pipe(imageResize({
-// 			width: 458 * 2,
-// 			height: 300 * 2,
-// 			crop : true
-// 		}))
-// 		.pipe(rename({ suffix: '-size' }))
-// 		.pipe(gulp.dest('src/img/name/size/'));
-// });
+const cutImg = () => {
+	return gulp.src('src/img/name/*.jpg')
+		.pipe(imageResize({
+			width: 458 * 2,
+			height: 300 * 2,
+			crop : true
+		}))
+		.pipe(rename({ suffix: '-size' }))
+		.pipe(gulp.dest('src/img/name/size/'));
+};
+exports.cutImg = cutImg;
 
-gulp.task('scss', () => {
+const scss = () => {
 	return gulp.src('src/scss/style.scss')
 		.pipe(plumber())
 		.pipe(sass())
@@ -39,9 +40,10 @@ gulp.task('scss', () => {
 		// .pipe(csso())
         // .pipe(rename({ suffix: '.min' }))
 		// .pipe(gulp.dest('dist/css/'))
-});
+};
+exports.scss = scss;
 
-gulp.task('js', () => {
+const js = () => {
 	return gulp.src('src/js/*.js')
 		.pipe(babel())
         .pipe(concat('scripts.js'))
@@ -50,9 +52,10 @@ gulp.task('js', () => {
         // .pipe(uglify())
         // .pipe(rename({ suffix: '.min' }))
         // .pipe(gulp.dest('dist/static/js'))
-});
+};
+exports.js = js;
 
-gulp.task('njk', () => {
+const njk = () => {
 	return gulp.src('src/njk/pages/**/*.njk')
         .pipe(nunjucks({
             path: ['src/njk/layouts']
@@ -61,9 +64,10 @@ gulp.task('njk', () => {
         .pipe(prettier({ proseWrap: 'never', printWidth: 800, tabWidth: 4, useTabs: true }))
         .pipe(gulp.dest('dist/'))
         .pipe(browserSync.stream());
-});
+};
+exports.njk = njk;
 
-gulp.task('img', () => {
+const img = () => {
 	return gulp.src('src/img/**/*.{jpg,png,svg}')
 		.pipe(imagemin([
 			imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -77,9 +81,10 @@ gulp.task('img', () => {
 		.pipe(gulp.dest('dist/img'))
 		.pipe(webp())
 		.pipe(gulp.dest('dist/img')); 
-});
+};
+exports.img = img;
 
-gulp.task('favicons', () => {
+const favicons = () => {
 	return gulp.src('src/img/**/*.{jpg,png,svg}')
 		.pipe(imagemin([
 			imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -93,23 +98,27 @@ gulp.task('favicons', () => {
 		.pipe(gulp.dest('dist/img'))
 		.pipe(webp())
 		.pipe(gulp.dest('dist/img')); 
-});
+};
+exports.favicons = favicons;
 
-gulp.task('fonts', () => {
+const fonts = () => {
 	return gulp.src('src/fonts/**/*{woff,woff2}')
         .pipe(gulp.dest('dist/fonts'));
-});
+};
+exports.fonts = fonts;
 
-gulp.task('settings', () => {
+const settings = () => {
 	return gulp.src('src/**/*{xml,json}')
         .pipe(gulp.dest('dist/'));
-});
+};
+exports.settings = settings;
 
-gulp.task('del', () => {
+const clear = () => {
 	return del('dist');
-});
+};
+exports.clear = clear;
 
-gulp.task('serve', () => {
+const serve = () => {
 	browserSync.init({
 		server: "dist",
 		routes: {},
@@ -122,30 +131,31 @@ gulp.task('serve', () => {
         }
     });
 
-	gulp.watch('src/img/**/*{jpg,png,svg}', gulp.series('img'));
-
     gulp.watch('src/scss/**/*.scss', gulp.series('scss'));
     gulp.watch('src/js/*.js', gulp.series('js'));
 	gulp.watch('src/njk/**/*.njk', gulp.series('njk'));
+	gulp.watch('src/img/**/*{jpg,png,svg}', gulp.series('img'));
 
-	gulp.watch('dist/**/*.js').on('change', browserSync.reload);
 	gulp.watch('dist/**/*{jpg,png,svg}').on('change', browserSync.reload);
-});
+};
+exports.serve = serve;
+exports.default = serve;
 
-gulp.task('build', gulp.series(
-	'del',
-	'scss',
-	'js',
-	'njk',
-	'fonts',
-	'img',
-	'favicons',
-	'settings'
-));
+const build = gulp.series(
+	clear,
+	gulp.parallel(
+		scss,
+		js,
+		njk,
+		fonts,
+		img,
+		favicons,
+		settings
+	),
+);
+exports.build = build;
 
-gulp.task('start', gulp.series(
-	'build',
-	'serve'
-));
-
-gulp.task('default', gulp.series('serve'));
+exports.start = gulp.series(
+	build,
+	serve
+);
